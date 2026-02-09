5334a0c5ca7aded447e4ea65d9d5bde498f94efc
lightning47 <Vignesh112847@gmail.com>
2026-02-09 18:12:44 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 395 ms (16.13%), Space: 53.2 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String max = "";
-        for(int i =0; i<s.length();i++){
-            for(int j =i;j<s.length();j++) {
-                String temp = s.substring(i, j+1);
-                if(plindrome(temp)) {
-                    if(max.length() < temp.length()) {
-                        max = temp;
-                    }
-                } 
+        String ans = "";
+        for(int i =0; i<s.length(); i++) {
+            String a = helper(i,i,s);
+            String b = helper(i,i+1,s);
+            if(a.length() > ans.length()) ans = a;
+            if(b.length() > ans.length()) ans = b;
+        } 
+        return ans;
+    }
+    private String helper(int i , int j, String s) {
+        String common = "";
+        String ans = "";
+
+        while(i>= 0 && j <s.length()) {
+            if(s.charAt(i) == s.charAt(j)) {
+                if(i == j) common = s.charAt(i)+"";
+                else ans+= s.charAt(i);
+
+                i--;
+                j++;
+            }
+            else {
+                break;
             }
         }
-        return max;
-    }
-    private boolean plindrome(String s) {
-        int r = s.length() -1;
-        int l = 0;
-
-        while (r>l) {
-            if(s.charAt(r) != s.charAt(l)) return false;
-            l++;r--;
+        String rev = "";
+        for(int k =ans.length()-1; k>=0;k--) {
+            rev += ans.charAt(k);
         }
-        return true;
+        ans = rev + common + ans;
+        return ans;
     }
 }
