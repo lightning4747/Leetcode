cf8c1abf962b93be0b8452fd264898116890a467
lightning4747 <Vignesh112847@gmail.com>
2026-03-12 22:36:14 +0530

    0152-MAXIMUM-PRODUCT-SUBARRAY Stats: Time: 294 ms (5.8%), Space: 47.3 MB (49.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,14 +3,12 @@ class Solution {
        int max = nums[0];
        int prod = 1;
        for(int i =0;i<nums.length;i++){
-        for(int j = i; j<nums.length;j++) {
+        for(int j =i;j<nums.length;j++) {
             prod *= nums[j];
             max = Math.max(max, prod);
-            if(prod == 0) prod = 1;
         }
         prod = 1;
        }
-    return max;
-
+       return max;
     }
 }
