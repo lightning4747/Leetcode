68231e1db7914440180c5536799006953776a5a0
lightning4747 <Vignesh112847@gmail.com>
2026-03-12 21:46:19 +0530

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 1 ms (99.81%), Space: 94.5 MB (68.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] nums) {
+        int min = Integer.MAX_VALUE;
         int max = 0;
-        int min = nums[0];
-        for(int i=0;i<nums.length;i++) {
+        for(int i =0;i<nums.length;i++) {
             min = Math.min(min, nums[i]);
             int profit = nums[i] - min;
-            max = Math.max(profit, max); 
+            max = Math.max(profit, max);
         }
         return max;
     }
