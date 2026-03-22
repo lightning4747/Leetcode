23ffb1ba470e9ce4678284574b7d87f7a5f1005a
lightning4747 <Vignesh112847@gmail.com>
2026-03-22 21:36:39 +0530

    0242-VALID-ANAGRAM Stats: Time: 17 ms (16.32%), Space: 46.6 MB (30.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
         if(s.length() != t.length()) return false;
-        
-        char[] ss = s.toCharArray();
-        char[] tt = t.toCharArray();
-        Arrays.sort(ss);
-        Arrays.sort(tt);
+        for(int i =0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0) + 1);
+        }
 
-        for(int i=0;i<ss.length;i++) {
-            if(ss[i] != tt[i]) return false;
+        for(int i=0;i<s.length();i++){
+            char ch = t.charAt(i);
+            if(!map.containsKey(ch) ||  map.get(ch) == 0) return false;
+            map.put(ch, map.get(ch)-1);
         }
         return true;
     }
