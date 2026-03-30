68714039001a550934549d0342154770a63856b7
lightning4747 <Vignesh112847@gmail.com>
2026-03-30 19:00:41 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 7 ms (75.17%), Space: 49.9 MB (33.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-
+        List<List<String>> res = new ArrayList<>();
         for(String str : strs) {
             char[] arr = str.toCharArray();
             Arrays.sort(arr);
             String key = new String(arr);
-            if(!map.containsKey(key)) map.put(key, new ArrayList<>());
-            map.get(key).add(str);
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+             map.get(key).add(str);
         }
-        return new ArrayList<>(map.values());
+
+        for(List<String> str : map.values()) {
+            res.add(str);
+        }
+        return res;
     }
 }
