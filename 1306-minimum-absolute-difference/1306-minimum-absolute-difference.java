f4ffceed60fd352380e8f5d02d8ebf6af0156fac
lightning4747 <Vignesh112847@gmail.com>
2026-04-24 19:16:49 +0530

    1306-MINIMUM-ABSOLUTE-DIFFERENCE Stats: Time: 23 ms (16.02%), Space: 63.9 MB (53.82%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
@@ -1,19 +1,16 @@
 class Solution {
-    public List<List<Integer>> minimumAbsDifference(int[] A) {
-        Arrays.sort(A);
-        int n = A.length;
-        int minDiff = Integer.MAX_VALUE;
+    public List<List<Integer>> minimumAbsDifference(int[] nums) {
+        int min = Integer.MAX_VALUE;
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
 
-        for (int i = 1; i < n; i++) {
-            int diff = A[i] - A[i - 1];
-            if (diff < minDiff) {
-                minDiff = diff;
-                res = new ArrayList<>();
-                res.add(Arrays.asList(A[i - 1], A[i]));
-            } else if (diff == minDiff) {
-                res.add(Arrays.asList(A[i - 1], A[i]));
-            }
+        for(int i =0;i<nums.length-1;i++) {
+            min = Math.min(min, Math.abs(nums[i]- nums[i+1]));
+        }
+
+        for(int i =0;i<nums.length - 1;i++) {
+            if(Math.abs(nums[i]- nums[i+1]) == min) res.add(new ArrayList<>(List.of(nums[i], nums[i+1])));
+
         }
 
         return res;
