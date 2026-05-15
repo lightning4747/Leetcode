88043466f9aa58218b1643457d3d427654374f8a
lightning4747 <Vignesh112847@gmail.com>
2026-05-15 18:36:41 +0530

    0046-PERMUTATIONS Stats: Time: 1 ms (86.81%), Space: 45.7 MB (17.16%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        recursion(0, nums.length, nums, list);
-        return list;
+       List<List<Integer>> list = new ArrayList<>();
+       recursion(0, nums, list);
+       return list; 
     }
 
-    private void recursion(int start, int end, int[] nums, List<List<Integer>> list) {
-        if(start == end) {
-            List<Integer> currentPermutation = new ArrayList<>();
-            for (int num : nums) currentPermutation.add(num);
-            list.add(currentPermutation);
+    private void recursion(int start, int[] nums, List<List<Integer>> list) {
+        if(start == nums.length) {
+            List<Integer> res = new ArrayList<>();
+            for(int num : nums) res.add(num);
+            list.add(new ArrayList<>(res));
             return;
         }
 
-        for(int i = start; i<end;i++) {
-            int temp = nums[start];
-            nums[start] = nums[i];
-            nums[i] = temp;
+        for(int i = start;i<nums.length;i++) {
+            int temp = nums[i];
+            nums[i] = nums[start];
+            nums[start] = temp;
 
-            recursion(start+1, end, nums, list);
+            recursion(start+1, nums, list);
 
-            temp = nums[start];
             nums[start] = nums[i];
             nums[i] = temp;
         }
