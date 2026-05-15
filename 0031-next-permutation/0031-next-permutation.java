5c98784c656a09e1369990acbeb78918f120244d
lightning4747 <Vignesh112847@gmail.com>
2026-05-15 19:20:51 +0530

    0031-NEXT-PERMUTATION Stats: Time: 0 ms (100%), Space: 44.6 MB (68.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,34 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 1;
-        while(i > 0 && nums[i-1] >= nums[i]) {
-            i--;
-        }
+
+        while(i > 0 && nums[i - 1] >= nums[i]) i--;
+
         if(i == 0) {
-            reverse(nums, 0 , nums.length - 1);
+            reverse(nums,i);
             return;
         }
-        int j = nums.length - 1;
-        while(j >= i && nums[j] <= nums[i-1] ) {
+        int j = nums.length-1;
+        while( j >= i && nums[j] <= nums[i-1]) {
             j--;
         }
-        swap(nums, i-1, j);
-        reverse(nums, i , nums.length - 1);
+
+        swap(i-1,j,nums);
+        reverse(nums,i);
     }
 
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+    void swap(int i , int j , int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
-    private void reverse(int[] nums, int start , int end) {
+    void reverse(int[] nums, int i) {
+        int j = nums.length - 1;
 
-        while(start < end) {
-            swap(nums,start, end);
-            start++;
-            end--;
+        while(i < j) {
+            swap(i, j , nums);
+            i++;j--;
         }
     }
 }
